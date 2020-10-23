bf6787bd0798da3af7fb6b01235f0fd424c29b04
Unknown <rgoforth@ceiamerica.com>
2020-10-22 23:14:38 -0400

    use not operator

--- Schedule.pl
+++ Schedule.pl
@@ -1,9 +1,8 @@
 :- use_module(library(lists)).
 
-% Select Element X, from List L, Returning the Rest of the List
-% Can I replace this with a built in function?
-el(X,[X|L],L).
-el(X,[_|L],R) :- el(X,L,R).
+% L is The subset of the list following X
+el(X,[X|L],L). %When X is the first item, return the list without X
+el(X,[_|L],R) :- el(X,L,R). %When X is not the first elemwnt, run again with the resto of the the list, looking to return the list of items after X 
 
 % selectN(N,L,S) :- select N elements of the list L and put them in 
 %    the set S. Via backtracking return all posssible selections, but
@@ -23,31 +22,16 @@ group(G,[N1|Ns],[G1|Gs]) :-
    subtract(G,G1,R),
    group(R,Ns,Gs).
 
-not_in(_,[]) :- !.
-not_in(X, [H|T]) :-
-   intersection(X,H,I),
-   same_length(X, I),
-   not_in(X,T).
-
-not_member(_, []) :- !.
-
-not_member([X|_], [H|T]) :-
-   %  write('Member to Check   '),
-   %  writeln(X),
-   %  write('Is in List    '),
-   %  writeln(H),
-    not_in(X,H) ,!, 
-    writeln('hit'),
-    not_member(X, T).
-    %not_member(Y, T).
-
 players([rich,joe,ralph,andrew,jeff,schultz,schoppie,marty,eman]).
 
-games(0,[],_Acc) :- !.
+games(0,[],_) :- !.
 games(I,[A|B],Acc) :- I > 0,
-                players(P),
-                group(P,[3,3,3],A), 
-                writeln(Acc),
-                not_member(A, Acc),
+                players(P), 
+                group(P,[3,3,3],A),
+                \+ member(A, Acc),
                 I1 is I-1,
                 games(I1,B,[A|Acc]).
+
+%aggregate_all(count, member(rich,[rich,andrew,ralph]), X).
+
+%games(3,[L,M,C],[]).
