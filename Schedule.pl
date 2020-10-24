d7b2913bde778ec46d674bcdfabe453dce32735e
Unknown <rgoforth@ceiamerica.com>
2020-10-24 12:54:45 -0400

    Work on counting game player totals

--- Schedule.pl
+++ Schedule.pl
@@ -1,5 +1,7 @@
 :- use_module(library(lists)).
 
+players([rich,joe,ralph,andrew,jeff,schultz,schoppie,marty,eman,ian]).
+
 % L is The subset of the list following X
 el(X,[X|L],L). %When X is the first item, return the list without X
 el(X,[_|L],R) :- el(X,L,R). %When X is not the first elemwnt, run again with the resto of the the list, looking to return the list of items after X 
@@ -22,14 +24,13 @@ group(G,[N1|Ns],[G1|Gs]) :-
    subtract(G,G1,R),
    group(R,Ns,Gs).
 
-players([rich,joe,ralph,andrew,jeff,schultz,schoppie,marty,eman,ian]).
 
 %Check in any member of the the list X exists in the list A
 not_in([],_).
 not_in([XH|XT],A) :- \+ memberchk(XH, A), not_in(XT,A).
 
 %Check that all items in List X are anywhere in a list of lists A
-all_in([],_).
+all_in([],_) :- !.
 all_in(X,[AH|AT]) :- is_list(AH),
                      intersection(X, AH, X)
                      ;
@@ -37,17 +38,24 @@ all_in(X,[AH|AT]) :- is_list(AH),
 
 %Split a list into pairs, and count how many times each pair appears in the list of combinations
 pairs_in(ListToCheck, ListOfCombinations, Count) :- selectN(2,ListToCheck,Pair), 
-                                                 aggregate_all(count, all_in(Pair,ListOfCombinations),Count).
+                                                    aggregate_all(count, all_in(Pair,ListOfCombinations),Count).
 
-%True if all the items in a numeric list are within Range of one another
-list_items_in_range([H|T], Range) :- list_items_in_range([H|T], Range, H, H). %The first min value is the first item in the list
-list_items_in_range([],_,_,_). %If the list is empty, items are always within the range                                             
-list_items_in_range([H|T], Range, Min, Max) :- Min1 is min(Min,H),
+played(X,Y) :- played(X,Y, []).
+played(_,[], _).
+played(X, [H|T], Lengths) :- is_list(H), memberchk(X,H), length(H, L), append(Lengths,L,L1), played(X,T,L1).
+
+%game_player_count([H|T],GameList,Result) :- fail.
+
+%True if all the items in a numeric list are within 'Range' of one another
+items_in_range([H|T], Range) :- items_in_range([H|T], Range, H, H). %The first min value is the first item in the list
+items_in_range([],_,_,_). %If the list is empty, items are always within the range                                             
+items_in_range([H|T], Range, Min, Max) :- Min1 is min(Min,H),
                                           Max1 is max(Max, H),
                                           Max1 =< Range+Min1, %max and min are still within the range                                          
-                                          list_items_in_range(T, Range, Min1, Max1).
+                                          items_in_range(T, Range, Min1, Max1).
 
 %Get the player combinations that satisfy the rules
+games(GamesToPlay) :- games(length(GamesToPlay), GamesToPlay, []).
 games(0,[],_) :- !.
 games(I,[A|B],Acc) :- I > 0,
                 players(P), 
@@ -55,13 +63,6 @@ games(I,[A|B],Acc) :- I > 0,
                 not_in(A, Acc),  %the new player sets don't contain any previous player sets
                 append(Acc,A,Acc1), %Add the new player sets to the full list of previous games
                 aggregate_all(bag(Count), pairs_in(P,Acc1,Count), ListOfReplayCounts),
-                min_list(ListOfReplayCounts, ReplayMin),
-                ReplayMax is ReplayMin + 1,
-                max_list(ListOfReplayCounts, ReplayMax),
-                %between(0, 2, SumCount),
+                items_in_range(ListOfReplayCounts, 1),
                 I1 is I-1, %move to the next game
                 games(I1,B,Acc1).
-
-%aggregate_all(count, member(rich,[rich,andrew,ralph]), X).
-
-%games(3,[L,M,C],[]).
