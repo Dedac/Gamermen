5393541231c0ef04b0c048059c93b410722d8358
Richard Goforth <rcgoforth@gmail.com>
2021-10-20 15:51:05 -0400

    Final for Season 3

--- MonteCarloSchedule/Program.cs
+++ MonteCarloSchedule/Program.cs
@@ -12,9 +12,23 @@ namespace MonteCarloSchedule
         static void Main(string[] args)
         {
             Set BestSet = BuildSet();
-            
-            if (args.Length > 0) //If there is a set file specified, start with that as the best set
-                BestSet = JsonSerializer.Deserialize<Set>(File.ReadAllText(args[0]));            
+
+            //If there is a set file specified, start with that as the best set
+            if (args.Length > 0)
+            {
+                BestSet = JsonSerializer.Deserialize<Set>(File.ReadAllText(args[0]));
+                //Rebuild player statistics if manually edited games
+                BestSet.Players.ForEach(p =>
+                {
+                    p.PlayerCount3s = 0;
+                    p.PlayerCount4s = 0;
+                    p.PlayedWith = new List<string>();
+                });
+                foreach (var game in BestSet.Months.SelectMany(m => m.Games))
+                {
+                    UpdatePlayerStatistics(game, BestSet);
+                }
+            }
 
             //Run as many in parallel as your processors support
             Parallel.For(0, 8, (int j) =>
@@ -33,7 +47,7 @@ namespace MonteCarloSchedule
 
                     var set = BuildSet();
                     bestSumCycle = Math.Max(bestSumCycle, set.PlayedWithSum);
-                    if  (set.PlayedWithSum > BestSet.PlayedWithSum ||
+                    if (set.PlayedWithSum > BestSet.PlayedWithSum ||
                         (
                             set.PlayedWithMax - set.PlayedWithMin < BestSet.PlayedWithMax - BestSet.PlayedWithMin
                             && set.PlayedWithSum >= BestSet.PlayedWithSum
@@ -47,72 +61,76 @@ namespace MonteCarloSchedule
             });
 
             BestSet.Display();
-            BestSet.Players.ForEach(p => Console.WriteLine($"{p.Name} - {string.Join(", ", GetNewPlayerList().Select(pl => pl.Name).Where(pl => pl != p.Name).Except(p.PlayedWith))}"));
+            BestSet.Players.ForEach(p => Console.WriteLine($"{p.Name} - {string.Join(", ", p.PlayedWith.GroupBy(x => x).Where(g => g.Count() > 2).Select(x => $"{x.Key}({x.Count()} Times)"))}"));
         }
 
         static void AddPlayersToGame(string player, Game game, List<string> players, Set currentSet)
         {
-            if (game.Players.Count == 4) return;
-            
-            game.Players.Add(player);
-            
-            var mp = currentSet.Players.FirstOrDefault(mp => mp.Name == player); 
+            if (player == null || game.Players.Count == 4) return; //run until we add 4 players to a game or there are no more players
 
-            var unplayed = players.Except(game.Players).ToList().Except(game.Players.SelectMany(pl => currentSet.Players.FirstOrDefault(mp => mp.Name == pl).PlayedWith));
-        
-            if (unplayed.Count() == 0) unplayed = players.Except(mp.PlayedWith).Except(game.Players);
-            
-            var nextunplayed = unplayed.FirstOrDefault() ?? players.First();
-            
+            game.Players.Add(player);
+
+            var currentPlayer = currentSet.Players.FirstOrDefault(mp => mp.Name == player);
+
+            var unplayed = players.Except(currentPlayer.PlayedWith).Except(game.Players);
+            if (unplayed.Count() == 0) //if there is no one unplayed, switch to players played only once
+                unplayed = players.Except(game.Players).Except(currentPlayer.PlayedWith.GroupBy(x => x).Where(g => g.Count() > 1).Select(x => x.Key));
+            if (unplayed.Count() == 0) //if there is still no one unplayed, switch to players played only twice
+                unplayed = players.Except(game.Players).Except(currentPlayer.PlayedWith.GroupBy(x => x).Where(g => g.Count() > 2).Select(x => x.Key));
+
+            var nextunplayed = unplayed.FirstOrDefault() ?? players.Where(mp => mp != player).FirstOrDefault();
+
             AddPlayersToGame(nextunplayed, game, players.Where(pl => pl != player).ToList(), currentSet);
         }
 
 
-
         static Set BuildSet()
         {
-            Set currentSet = new Set();
+            var currentSet = new Set();
             currentSet.Months = GetNewMonthList();
             currentSet.Players = GetNewPlayerList();
+
 
             foreach (var m in currentSet.Months)
             {
                 var players = GetNewPlayerList().Select(p => p.Name).ToList();
-
+                var totalGames = players.Count / 4; //integer division
                 players.Shuffle();
 
-                for (int i = 0; i < 4; i++) //Build 4 games
+                for (int i = m.Games.Count; i < totalGames; i++)
                 {
-                    var playercount = 4; //no longer using 
-                    var game = new Game() { }; //Get the next set of players for this game
+                    var game = new Game();
 
                     AddPlayersToGame(players.First(), game, players, currentSet);
 
                     m.Games.Add(game);
                     //remove players added to this set from future games
                     players.RemoveAll(p => game.Players.Contains(p));
-
-                    game.Players.ForEach(p => //Add game to each player's info
-                    {
-                        var mp = currentSet.Players.FirstOrDefault(mp => mp.Name == p);
-                        if (playercount == 3)
-                            mp.PlayerCount3s++;
-                        else if (playercount == 4)
-                            mp.PlayerCount4s++;
-
-                        mp.PlayedWith.AddRange(game.Players.Where(a => a != mp.Name));
-                    });
+                    UpdatePlayerStatistics(game, currentSet);
                 }
             }
 
             return currentSet;
         }
 
+        static void UpdatePlayerStatistics(Game game, Set currentSet)
+        {
+            game.Players.ForEach(p =>
+            {
+                var currentPlayer = currentSet.Players.FirstOrDefault(mp => mp.Name == p);
+                if (game.Players.Count <= 3)
+                    currentPlayer.PlayerCount3s++;
+                else
+                    currentPlayer.PlayerCount4s++;
+
+                currentPlayer.PlayedWith.AddRange(game.Players.Where(a => a != currentPlayer.Name));
+            });
+        }
         static List<Player> GetNewPlayerList() =>
-             JsonSerializer.Deserialize<Set>(File.ReadAllText("./MonthAndPlayerInitial.json")).Players;
+                JsonSerializer.Deserialize<Set>(File.ReadAllText("./MonthAndPlayerInitial.json")).Players;
 
         static List<Month> GetNewMonthList() =>
-            JsonSerializer.Deserialize<Set>(File.ReadAllText("./MonthAndPlayerInitial.json")).Months;
+                JsonSerializer.Deserialize<Set>(File.ReadAllText("./MonthAndPlayerInitial.json")).Months;
 
         static List<(Player, Player, int)> PlayerPairsPlayed() => new List<(Player, Player, int)>();
     }
